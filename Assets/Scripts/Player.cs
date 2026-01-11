5eb81cdd1afce93f826f4d82c3ee6c63bd98413f
minjoos5 <minjoos5@uci.edu>
2026-01-11 14:01:52 -0800

    MG1 finished

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,18 +11,41 @@ public class Player : MonoBehaviour
     private int _numSeedsLeft;
     private int _numSeedsPlanted;
 
+
+
     private void Start ()
     {
+       _numSeedsLeft = _numSeeds;
+       _numSeedsPlanted = 0;
+       _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
         
     }
 
     private void Update()
     {
+        float _xval = Input.GetAxis("Horizontal");
+        float _yval = Input.GetAxis("Vertical");
+
+        Vector3 _movement = new Vector3(_xval, _yval, 0f);
+        transform.position += _movement * _speed * Time.deltaTime;
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_numSeedsLeft > 0)
+            {
+                PlantSeed();
+            }   
+        }
         
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 
     public void PlantSeed ()
     {
-        
+        GameObject _seed = Instantiate(_plantPrefab);
+        _seed.transform.position = transform.position;
+        _numSeedsLeft--;
+        _numSeedsPlanted++;
     }
 }
